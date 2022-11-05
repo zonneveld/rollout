854697ab59551d4113a636ff3ca16f66b27e99ac
zonneveld <zdj2606@novacollege.nl>
2022-11-05 16:29:12 +0100

    added i2ctools to installer

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 apt-get update -y &&
 apt-get install python3-pip -y &&
+apt-get install i2c-tools &&
 pip install netifaces &&
 pip install adafruit-circuitpython-motorkit &&
 pip install adafruit-circuitpython-ssd1306 &&
